c432655c60a883530a9240521183847e6f8c2968
Shuvam B.C <bcshuvam@gmail.com>
2023-03-15 10:41:36 +0545

    Lab 11

---
+++ lab-11/fibonacciSeriesRecursion.c
@@ -0,0 +1,31 @@
+// LAB 11
+// Q3. Display the first 10 fibonacci series using recursion.
+
+#include <stdio.h>
+
+int fib_recursive(int n);
+
+int main()
+{
+    int number = 10;
+
+    printf("The first 10 fibonacci series = %d\n", fib_recursive(number));
+
+    getch();
+    return 0;
+}
+
+int fib_recursive(int n)
+{
+    if (n == 1 || n == 2)
+    {
+        return n - 1;
+    }
+    else
+    {
+        return fib_recursive(n - 1) + fib_recursive(n - 2);
+    }
+}
+
+// Output :
+//      The first 10 fibonacci series = 34
